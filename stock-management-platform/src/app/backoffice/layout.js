494363f654b5d24197a4efb5835eb2de9fa1bd3a
marcuscabral21 <marcusdavicabral2101@gmail.com>
2025-04-26 04:02:36 +0100

    Criei a dashboard inicial + config proejto

---
+++ stock-management-platform/src/app/backoffice/layout.js
@@ -0,0 +1,42 @@
+export const metadata = {
+    title: 'Backoffice - Inventory Management', // Set the title of the page
+}
+
+export default function BackofficeLayout({ children }) {
+return (
+    <div className="flex min-h-screen bg-gray-130">
+        {/* Sidebar */}
+        <div className="w-64 bg-gray shadow-md">
+            <div className="p-4 border-b">
+                <h1 className="text-l font-bold font-mono">Inventory Management</h1>
+            </div>
+
+        <nav className="p-2">
+            <ul className="space-y-1">
+                <li>
+                    <a
+                        href="/backoffice"
+                        className="block px-4 py-2 rounded hover:bg-gray-700 font-mono"
+                    >
+                        Dashboard
+                    </a>
+                </li>
+                <li>
+                    <a
+                        href="/backoffice/stock"
+                        className="block px-4 py-2 rounded hover:bg-gray-700 font-mono"
+                    >
+                        Stock
+                    </a>
+                </li>
+            </ul>
+        </nav>
+    </div>
+
+    {/* Main content */}
+    <div className="flex-1 p-6">
+        {children}
+    </div>
+    </div>
+)
+}
---
+++ stock-management-platform/src/app/backoffice/page.js
@@ -0,0 +1,14 @@
+export default function BackofficePage() {
+    return (
+      <div className="pt-0 p-6">
+        <div className="flex items-center justify-between mb-8">
+          <h2 className="text-2xl font-semibold font-mono">Dashboard</h2>
+        </div>
+        
+        <div className="bg-black p-8 rounded-lg shadow w-full max-w-2xl mx-auto">
+          <p className="text-lg mb-4 font-mono">Welcome to Inventory Manager Dashboard.</p>
+          <p className="text-lg font-mono">Select "Stock" from the side bar to manage your products.</p>
+        </div>
+      </div>
+    )
+  }
